fa7d5816e807684ec934555fdbef536e97d7dac1
bina <sburhanova04@gmail.com>
2024-08-07 20:12:38 +0500

    final

---
+++ main.js
@@ -0,0 +1,30 @@
+//a
+// var name = prompt("Введите своё  имя")
+// console.log("Меня зовут " + name);
+
+// var age = +prompt("Введите свой возраст")
+// console.log("Мне " + age);
+
+// var ex1= +prompt ("Решите пример: 10 + 5 = ? ")
+// console.log("Пример 1: 10 + 5 = 15. Ваш ответ: " + ex1);
+
+// var ex2= +prompt ("Решите пример: 10 - 5 = ? ")
+// console.log("Пример 2: 10 - 5 = 10. Ваш ответ: " + ex2);
+
+// var ex3= +prompt ("Решите пример: 10 * 5 = ?")
+// console.log("Пример 3: 10 * 5 = 50. Ваш ответ: " + ex3);
+
+// var ex4= +prompt ("Решите пример: 10 / 5 = ?")
+// console.log("Пример 4: 10 / 5 = 2. Ваш ответ: " + ex4);
+
+// var ex5= +prompt ("Решите пример: 10 % 3 = ?")
+// console.log("Пример 5: 10 % 3 = 1. Ваш ответ: " + ex4);
+// alert("Зайдите в консоль")
+
+//b
+
+var X = +prompt (" X:")
+var Y = +prompt (" Y:")
+var Z = +prompt (" Z:")
+var res= (X+Y+Z)/3;
+alert("Среднее арифметическое чисел = " + res)
